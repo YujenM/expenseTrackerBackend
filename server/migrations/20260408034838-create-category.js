34848c040f4b7a35079a2b8a3a234f85598a779d
YujenM <maharjanyuzen@gmail.com>
2026-04-19 13:20:36 +0545

    category type added

--- server/migrations/20260408034838-create-category.js
+++ server/migrations/20260408034838-create-category.js
@@ -24,11 +24,9 @@ module.exports = {
       },
 
       createdAt: {
-        allowNull: false,
         type: Sequelize.DATE,
       },
       updatedAt: {
-        allowNull: false,
         type: Sequelize.DATE,
       },
     });
